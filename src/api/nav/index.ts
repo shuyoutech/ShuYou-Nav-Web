f7ddb3cf7751a2ea849e9789a894eaa08c648dca
YangChao <1219191915@qq.com>
2025-11-05 11:32:18 +0800

    add: init project

--- src/api/nav/index.ts
+++ src/api/nav/index.ts
@@ -1,6 +1,14 @@
 import type {AxiosResponse} from "axios";
 import api from "@/utils/axios.ts";
-import type {NavWebsiteQuery, NavWebsiteSaveBo, NavWebsiteUpdateBo, NavWebsiteVo} from "@/api/nav/types.ts";
+import type {
+  NavCustomCategoryBo,
+  NavCustomCategoryQuery,
+  NavCustomCategoryVo, NavCustomWebsiteBo, NavCustomWebsiteQuery, NavCustomWebsiteVo,
+  NavWebsiteQuery,
+  NavWebsiteSaveBo,
+  NavWebsiteUpdateBo,
+  NavWebsiteVo
+} from "@/api/nav/types.ts";
 
 /**
  * 网址管理-分类接口
@@ -61,6 +69,115 @@ export function navWebsiteUpdateApi(data: NavWebsiteUpdateBo): Promise<AxiosResp
 export function navWebsiteDeleteApi(data: string[]): Promise<AxiosResponse<boolean>> {
   return api({
     url: '/nav/website/delete',
+    method: 'post',
+    data: data
+  });
+}
+
+
+/**
+ * 自定义分类-树
+ */
+export function navCustomCategoryTreeApi(data: NavCustomCategoryQuery): Promise<AxiosResponse<Tree<NavCustomCategoryVo>[]>> {
+  return api({
+    url: '/nav/custom/category/tree',
+    method: 'post',
+    data: data
+  });
+}
+
+/**
+ * 自定义分类-详情
+ */
+export function navCustomCategoryDetailApi(categoryId: string): Promise<AxiosResponse<NavCustomCategoryVo>> {
+  return api({
+    url: '/nav/custom/category/detail/' + categoryId,
+    method: 'post'
+  });
+}
+
+/**
+ * 自定义分类-新增
+ */
+export function navCustomCategorySaveApi(data: NavCustomCategoryBo): Promise<AxiosResponse<number>> {
+  return api({
+    url: '/nav/custom/category/save',
+    method: 'post',
+    data: data
+  });
+}
+
+/**
+ * 自定义分类-修改
+ */
+export function navCustomCategoryUpdateApi(data: NavCustomCategoryBo): Promise<AxiosResponse<boolean>> {
+  return api({
+    url: '/nav/custom/category/update',
+    method: 'post',
+    data: data
+  });
+}
+
+/**
+ * 自定义分类-删除
+ */
+export function navCustomCategoryDeleteApi(data: string[]): Promise<AxiosResponse<boolean>> {
+  return api({
+    url: '/nav/custom/category/delete',
+    method: 'post',
+    data: data
+  });
+}
+
+/**
+ * 自定义网址-列表接口
+ */
+export function navCustomWebsiteApi<T = any>(data: NavCustomWebsiteQuery): Promise<AxiosResponse<T[]>> {
+  return api({
+    url: '/nav/custom/website/list',
+    method: 'post',
+    data: data
+  });
+}
+
+/**
+ * 自定义网址-详情
+ */
+export function navCustomWebsiteDetailApi(websiteId: string): Promise<AxiosResponse<NavCustomWebsiteVo>> {
+  return api({
+    url: '/nav/custom/website/detail/' + websiteId,
+    method: 'post'
+  });
+}
+
+/**
+ * 自定义网址-新增
+ */
+export function navCustomWebsiteSaveApi(data: NavCustomWebsiteBo): Promise<AxiosResponse<number>> {
+  return api({
+    url: '/nav/custom/website/save',
+    method: 'post',
+    data: data
+  });
+}
+
+/**
+ * 自定义网址-修改
+ */
+export function navCustomWebsiteUpdateApi(data: NavCustomWebsiteBo): Promise<AxiosResponse<boolean>> {
+  return api({
+    url: '/nav/custom/website/update',
+    method: 'post',
+    data: data
+  });
+}
+
+/**
+ * 自定义网址-删除
+ */
+export function navCustomWebsiteDeleteApi(data: string[]): Promise<AxiosResponse<boolean>> {
+  return api({
+    url: '/nav/custom/website/delete',
     method: 'post',
     data: data
   });
--- src/api/nav/types.ts
+++ src/api/nav/types.ts
@@ -26,3 +26,56 @@ export interface NavWebsiteSaveBo {
 export interface NavWebsiteUpdateBo extends NavWebsiteSaveBo {
   id: string;
 }
+
+export interface NavCustomCategoryQuery {
+  userId?: string;
+  name?: string;
+  parentId?: string;
+}
+
+export interface NavCustomCategoryVo {
+  id?: string;
+  userId?: string;
+  userName?: string
+  name?: string;
+  rootId?: string;
+  parentId?: string;
+  level?: number;
+  icon?: string;
+  sort?: number;
+}
+
+export interface NavCustomCategoryBo {
+  id?: string;
+  name?: string;
+  parentId?: string;
+  level?: number;
+  icon?: string;
+  sort?: number;
+}
+
+export interface NavCustomWebsiteQuery {
+  userId?: string;
+  name?: string;
+  categoryId?: string;
+}
+
+export interface NavCustomWebsiteVo {
+  id?: string;
+  userId?: string;
+  userName?: string
+  categoryId?: string;
+  name?: string;
+  url?: string;
+  icon?: string;
+  sort?: number;
+}
+
+export interface NavCustomWebsiteBo {
+  id?: string;
+  categoryId?: string;
+  name?: string;
+  url?: string;
+  icon?: string;
+  sort?: number;
+}
